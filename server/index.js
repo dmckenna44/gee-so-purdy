ee16453213c9bf8d5387d71caddc267849be7519
dmckenna44 <dmckenna05@gmail.com>
2023-02-06 23:16:05 -0800

    change base endpoint in server

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.set('trust proxy', 1);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
 });
 } else {
