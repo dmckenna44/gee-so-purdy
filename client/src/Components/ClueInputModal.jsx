d174a65f4d24fc2349ca1c5fc8b56ba4f39d9bde
dmckenna44 <dmckenna05@gmail.com>
2023-11-16 18:53:16 -0800

    add editorjs to format questions and answers

--- client/src/Components/ClueInputModal.jsx
+++ client/src/Components/ClueInputModal.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import tinymce from 'tinymce';
 import { SET_CURRENT_Q, SET_CURRENT_A, UPDATE_CLUE } from '../constants/actionTypes';
 import { updateGame, loadGames } from '../reducers/gameReducer.js';
-import tinymce from "tinymce";
+import TextEditor from "./TextEditor.jsx";
 
+const tinyConfig = {
+  selector: '.text-editor',
+  menubar: false,
+  inline: true,
+  plugins: [
+    'link', 'lists', 'powerpaste',
+    'autolink', 'tinymcespellchecker'
+  ],
+  toolbar: [
+    'undo redo | bold italic underline | fontfamily fontsize',
+    'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
+  ],
+  valid_elements: 'p[style],strong,em,span[style],a[href],ul,ol,li',
+  valid_styles: {
+    '*': 'font-size,font-family,color,text-decoration,text-align'
+  },
+  powerpaste_word_import: 'clean',
+  powerpaste_html_import: 'clean',
+}
 
 const ClueInputModal = (props) => {
+
+  // useEffect(() => {
+  //   tinymce.init(tinyConfig);
+  // }, [])
 
   const {handleModal, hidden} = props;
   const dispatch = useDispatch();
   const { currentIndex, currentQuestion, currentAnswer } = useSelector(state => state.game);
+  const [image, setImage] = useState('');
   
   const submitClue = (e) => {
     const newClue = {
@@ -23,6 +48,20 @@ const ClueInputModal = (props) => {
     handleModal(e);
   };
 
+  const addMediaClue = (e) => {
+    e.preventDefault();
+
+  }
+
+
+  const setMediaFile = (e) => {
+    console.log('file upload', e.target.files);
+    const fileURL = URL.createObjectURL(e.target.files[0]);
+    console.log(fileURL)
+    setImage(fileURL);
+
+  }
+
   const handleFocus = (e) => e.target.select();
 
   if (hidden) return null;
@@ -31,12 +70,16 @@ const ClueInputModal = (props) => {
       <h2>Add a New Clue</h2>
       <div className="inputDisplay">
         <h3>Question</h3>
-        <textarea id="input-question" rows={5} cols={40} onFocus={handleFocus} value={currentQuestion} onChange={(e) => dispatch({type: SET_CURRENT_Q, payload: e.target.value})}/>
+        <TextEditor type="question" id="question-editor" className="text-editor" />
+        {/* <input type="file" onChange={setMediaFile} /> */}
+        {image ? <img src={image}/> : ''}
       </div>
       <div className="inputDisplay">
         <h3>Answer</h3>
-        <input type="text" id="input-answer" onFocus={handleFocus} value={currentAnswer} onChange={(e) => dispatch({type: SET_CURRENT_A, payload: e.target.value})} />
+        <TextEditor type="answer" id="answer-editor" />
+        {/* <input type="text" id="input-answer" onFocus={handleFocus} value={currentAnswer} onChange={(e) => dispatch({type: SET_CURRENT_A, payload: e.target.value})} /> */}
       </div>
+      {/* <button onClick={addMediaClue}>Add Picture/Video/Audio</button> */}
       <div className="clue-input-btns">
         <button type="button" onClick={submitClue}>Submit</button>
         <button className="close-btn" onClick={handleModal}>Close</button>
---
+++ client/src/Components/TextEditor.jsx
@@ -0,0 +1,72 @@
+
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import EditorJS from "@editorjs/editorjs";
+import Underline from '@editorjs/underline';
+import { SET_CURRENT_A, SET_CURRENT_Q } from "../constants/actionTypes";
+
+
+const TextEditor = ({type, id}) => {
+
+
+
+  const ejInstance = useRef();
+  // const editorRef = useRef();
+
+  const { currentQuestion, currentAnswer } = useSelector(state => state.game);
+  const dispatch = useDispatch();
+
+  const INITIAL_DATA = {
+    "time": new Date().getTime(),
+    "blocks": [
+      {
+        "type": "paragraph",
+        "data": {
+          "text": `${type === 'question' ? currentQuestion : currentAnswer}`
+        }
+      }
+    ]
+  }
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holder: id,
+      onReady: () => {
+        ejInstance.current = editor
+      },
+      autfocus: true,
+      data: INITIAL_DATA,
+      inlineToolbar: ['bold', 'underline', 'italic', 'link'],
+      onChange: async () => {
+        let content = await editor.saver.save();
+        if (type === 'question') {
+          dispatch({type: SET_CURRENT_Q, payload: content.blocks[0].data.text})
+        } else dispatch({type: SET_CURRENT_A, payload: content.blocks[0].data.text})
+      },
+      tools: {
+        underline: Underline
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!ejInstance.current) {
+      initEditor();
+    }
+
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    }
+  }, [])
+
+  return (
+    <>
+      <div id={id} className="text-editor"></div>
+    </>
+  
+  )
+}
+
+
+export default TextEditor;
